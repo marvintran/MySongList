c6b1c5fdc676e5861fd6aa20aa91a2fc0541125f
marvintran <marvintran1234@gmail.com>
2020-10-27 18:05:11 -0500

    Changed logic of this components return. Instead of returning map results, return an array populated by forEach.

--- src/MonthList.js
+++ src/MonthList.js
@@ -3,11 +3,15 @@ import Songs from './Songs'
 
 class MonthList extends React.Component {
   render() {
+
+    const songs = [];
+    this.props.songs.forEach((song) => {
+      songs.push(<Songs song={song} filterText={this.props.filterText}/>);
+    });
+
     return (
       <ul>
-        {this.props.songs.map((song) => {
-          return <Songs song={song} filterText={this.props.filterText}/>
-        })}
+        {songs}
       </ul>
     )
   }
