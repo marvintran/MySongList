a77707207c4f6ee901700cdbedc968fee54d101c
marvintran <marvintran1234@gmail.com>
2020-10-28 15:43:32 -0500

    Change .forEach() to use .map() instead.

--- src/MonthList.js
+++ src/MonthList.js
@@ -4,9 +4,8 @@ import Songs from './Songs'
 class MonthList extends React.Component {
   render() {
 
-    const songs = [];
-    this.props.songs.forEach((song) => {
-      songs.push(<Songs song={song} filterText={this.props.filterText}/>);
+    const songs = this.props.songs.map((song) => {
+      return <Songs song={song} filterText={this.props.filterText}/>;
     });
 
     return (
