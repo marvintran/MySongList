c6f7d2dfeba65935ebd36dd09bd3da12b9bd66c6
marvintran <marvintran1234@gmail.com>
2020-11-10 22:33:18 -0600

    Added logic to create a button with an X symbol for buttons in the current filters.

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
+import { BiX } from "react-icons/bi";
 
-const FilterButton = ({ action, active, title, size, className }) => {
+const FilterButton = ({ action, active, title, size, className, currentFilter }) => {
   let variant = "outline-primary";
+
+  if(currentFilter) {
+    return (
+      <Button
+        onClick={() => action(title)}
+        variant={variant}
+        active={active}
+        size={size}
+        className={className}
+      >
+        {title}<BiX/>
+      </Button>
+    )
+  }
+
   return (
     <Button
       onClick={() => action(title)}
